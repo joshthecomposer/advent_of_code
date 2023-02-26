faaa8ce52cf9a11829a3ee780e86d5660482d858
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-02-26 03:14:10 -0500

    Setting up Day 5

---
+++ 05_SupplyStacks/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+int divider = Array.IndexOf(lines, string.Empty);
+int numStacks = int.Parse(lines[divider - 1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries).Last());
+Console.WriteLine(numStacks);
+
+List<char>[] stacks = new List<char>[numStacks];
+
+for (int i = Array.IndexOf(lines, lines[divider - 2]); i >= 0; i--)
+{
+    
+}
+
+// string[] temp = lines[3].Replace("[", "").Replace("]", "").Trim().Split(" ");
+// foreach(string s in temp)
+// {
+//     Console.WriteLine(s);
+// }
