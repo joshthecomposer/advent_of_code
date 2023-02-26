6b78c42ace85837e1f1e8e45e29e0f507487eb2f
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-02-26 00:21:34 -0500

    Day 4 Part 1 Complete

---
+++ 04_CampCleanup/Program.cs
@@ -0,0 +1,22 @@
+﻿//Part 01:
+
+int score = 0;
+
+foreach (string l in System.IO.File.ReadAllLines("input.txt"))
+{
+    int[] arr = new int[4];
+    string[] tt = l.Split(",");
+    arr[0] = Int32.Parse(tt[0].Split("-")[0]);
+    arr[1] = Int32.Parse(tt[0].Split("-")[1]);
+    arr[2] = Int32.Parse(tt[1].Split("-")[0]);
+    arr[3] = Int32.Parse(tt[1].Split("-")[1]);
+    if ((arr[0] <= arr[2] && arr[3] <= arr[1]) || (arr[2] <= arr[0] && arr[1] <= arr[3]))
+    {
+        score++;
+    }
+}
+
+Console.WriteLine(score);
+
+//Part 02:
+
