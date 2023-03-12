338295f25db04c2926ff538313abf730e0505301
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-03-12 01:12:15 -0500

    Day 06 complete

---
+++ 06_Tuning_Trouble/Program.cs
@@ -0,0 +1,24 @@
+﻿string input = File.ReadAllLines("input.txt")[0];
+
+int cursor = 0;
+int output = 0;
+while (output == 0)
+{
+    Dictionary<char, int> tempDick = new Dictionary<char, int>();
+    for (int i = cursor; i < cursor+14; i++)
+    {
+        if (tempDick.ContainsKey(input[i]))
+        {
+            break;
+        }
+        tempDick.Add(input[i], 1);
+    }
+    if (tempDick.Keys.Count == 14)
+    {
+        output = cursor+14;
+        break;
+    }
+    cursor++;
+}
+
+Console.WriteLine(output);
