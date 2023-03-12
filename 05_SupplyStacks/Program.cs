b16b4eebfd99fc38a7912f78a4278344e3cc200b
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-03-11 23:25:02 -0500

    Day5 Part1 complete

--- 05_SupplyStacks/Program.cs
+++ 05_SupplyStacks/Program.cs
@@ -1,17 +1,59 @@
 ﻿string[] lines = File.ReadAllLines("input.txt");
 int divider = Array.IndexOf(lines, string.Empty);
 int numStacks = int.Parse(lines[divider - 1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries).Last());
-Console.WriteLine(numStacks);
 
-List<char>[] stacks = new List<char>[numStacks];
+List<Stack<char>> stacks = new List<Stack<char>> {};
+int stackCursor = 1;
 
-for (int i = Array.IndexOf(lines, lines[divider - 2]); i >= 0; i--)
+//TODO: find the end size of stackcursor programmatically.
+while (stackCursor < 37)
 {
-    
+    Stack<char> temp = new Stack<char>();
+    for (int i = Array.IndexOf(lines, lines[divider-2]); i >= 0; i--)
+    {
+        if (lines[i][stackCursor] != ' ')
+        {
+            temp.Push(lines[i][stackCursor]);
+        }
+    }
+    stacks.Add(temp);
+    stackCursor += 4;
 }
 
-// string[] temp = lines[3].Replace("[", "").Replace("]", "").Trim().Split(" ");
-// foreach(string s in temp)
-// {
-//     Console.WriteLine(s);
-// }
+List<List<int>> workOrder = new List<List<int>>();
+
+//TODO: The output of this can be put directly into a function instead of instantiated first???
+for (int i = Array.IndexOf(lines, lines[divider + 1]); i < lines.Count(); i++)
+{
+    List<int> temp = new List<int>();
+    foreach (string s in lines[i].Split(" "))
+    {
+        int tempInt;
+        if (Int32.TryParse(s, out tempInt))
+        {
+            temp.Add(tempInt);
+        }
+    }
+    workOrder.Add(temp);
+}
+
+void TheGreatCrane(int moves, int from, int to)
+{
+    for (int i = 0; i < moves; i++)
+    {
+        // stacks[to-1].Push(stacks[from-1].Pop());
+    }
+}
+
+foreach(List<int> w in workOrder)
+{
+    TheGreatCrane(w[0], w[1], w[2]);
+}
+
+string output = "";
+foreach(Stack<char> s in stacks)
+{
+    output += s.First();
+}
+
+Console.WriteLine(output);
