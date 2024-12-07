4432de66cb85c330d3a508a706c8170945d69ff0
joshthecomposer <terminatormunky@gmail.com>
2024-12-07 12:56:42 -0600

    Add Day 3 part 2

--- 2024/day_03_mull_it_over/src/bin/part2.rs
+++ 2024/day_03_mull_it_over/src/bin/part2.rs
@@ -1,4 +1,5 @@
-use std::{any::Any, time::Instant};
+use std::time::Instant;
+use regex::{Match, Regex};
 
 fn main() {
     let input = include_str!("./input.txt");
@@ -9,8 +10,37 @@ fn main() {
     println!("Executed in: {:?}", duration);
 }
 
-fn find_solution(input: &str) -> isize {
-    0
+fn find_solution(input: &str) -> u32 {
+    let re = Regex::new(r"(do\(\))|(don't\(\))|(mul\(\d+,\d+\))").unwrap();
+
+    let re2 = Regex::new(r"(?<lhs>\d+),(?<rhs>\d+)").unwrap();
+    let mut result: u32 = 0;
+
+    let matches: Vec<&str> = re.find_iter(input).map(|c| c.as_str()).collect();
+
+    let mut should_do = true;
+
+    println!("{:?}", &matches);
+    
+    for m in matches {
+        match m {
+            "do()" => {
+                should_do = true;
+            },
+            "don't()" => {
+                should_do = false;
+            },
+            _=> {
+                if should_do {
+                    let cs = re2.captures(m).unwrap();
+                    result += cs.name("lhs").unwrap().as_str().parse::<u32>().unwrap() * cs.name("rhs").unwrap().as_str().parse::<u32>().unwrap();
+                }
+            }
+        }
+
+    }
+result
+
 }
 
 #[cfg(test)]
@@ -19,13 +49,8 @@ mod tests {
     #[test]
     fn it_works() {
         let result = find_solution(
-"7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9"
+"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 );
-        assert_eq!(4, result);
+        assert_eq!(48, result);
     }
 }
