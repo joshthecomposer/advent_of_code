f93efff5053ac06e44b0b1f18003d13545839854
joshthecomposer <terminatormunky@gmail.com>
2024-12-07 16:51:28 -0600

    Clear part2

--- 2024/day_04_ceres_search/src/bin/part2.rs
+++ 2024/day_04_ceres_search/src/bin/part2.rs
@@ -1,4 +1,4 @@
-use std::{any::Any, time::Instant};
+use std::time::Instant;
 
 fn main() {
     let input = include_str!("./input.txt");
@@ -10,54 +10,7 @@ fn main() {
 }
 
 fn find_solution(input: &str) -> isize {
-    let mut total_safe = 0;
-
-    for l in input.lines() {
-        let mut report:Vec<isize> = l.split_whitespace()
-            .filter_map(|n| n.parse().ok() )
-            .collect();
-
-        if is_safe(&report) {
-            total_safe += 1;
-            continue;
-        }
-
-        for i in 0..report.len() {
-            let n = report.remove(i);
-
-            if is_safe(&report) {
-                total_safe += 1;
-                break;
-            }
-
-            report.insert(i, n);
-        }
-    }
-   total_safe
-}
-
-fn is_safe(input: &Vec<isize>) -> bool {
-        let mut decreased = false;
-        let mut increased = false;
-
-        for (i, n) in input.iter().enumerate() {
-
-            if (i + 1) >= input.len() { continue; }
-
-            let change = n - input[i + 1]; 
-
-            if change.abs() > 3 || change.abs() == 0 { 
-                return false;
-            }
-
-            if change.is_positive() { decreased = true; }
-            if change.is_negative() { increased = true; }
-
-            if decreased && increased { 
-                return false;
-            }
-        }
-        true
+    0
 }
 
 #[cfg(test)]
@@ -66,12 +19,7 @@ mod tests {
     #[test]
     fn it_works() {
         let result = find_solution(
-"7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9"
+""
 );
         assert_eq!(4, result);
     }
